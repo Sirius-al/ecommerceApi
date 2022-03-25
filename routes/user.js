c2627e09e11fadf770225952a7d512bc0f08669f
Sabbir Ahmed <asabbir311@gmail.com>
2022-03-25 22:32:18 +0600

    2

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const CryptoJS = require("crypto-js");
-const User = require("../models/user");
+const User = require("../models/User");
 const {
     verifyToken,
     verifyTokenAndAuthorization,
